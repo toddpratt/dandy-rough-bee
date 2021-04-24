20e9f67d10ef05f00b309d144e47479204c80723
Glitch (hello-express) <none>
2021-04-24 07:55:51 +0000

    🍥👝 Checkpoint ./package.json:377298/2634 ./server.js:377298/1734

--- server.js
+++ server.js
@@ -1,29 +1,23 @@
-// server.js
-// where your node app starts
-
-// we've started you off with Express (https://expressjs.com/)
-// but feel free to use whatever libraries or frameworks you'd like through `package.json`.
 const express = require("express");
+const { Sequelize, Model, DataTypes } = require('sequelize');
+const bcrypt = require('bcrypt');
+
 const app = express();
+const sequelize = new Sequelize('sqlite::memory:');
 
-// our default array of dreams
-const dreams = [
-  "Find and count some sheep",
-  "Climb a really tall mountain",
-  "Wash the dishes"
-];
+class User extends Model {}
+User.init({
+  username: DataTypes.STRING,
+  password: DataTypes.STRING
+}, { sequelize, modelName: 'user' });
 
-// make all the files in 'public' available
-// https://expressjs.com/en/starter/static-files.html
 app.use(express.static("public"));
 
-// https://expressjs.com/en/starter/basic-routing.html
 app.get("/", (request, response) => {
   response.sendFile(__dirname + "/views/index.html");
 });
 
-// send the default array of dreams to the webpage
-app.get("/dreams", (request, response) => {
+app.post("/api/v1/login", (request, response) => {
   // express helps us take JS objects and send them as JSON
   response.json(dreams);
 });
