e5eb5b653627f143a25ca0b60d0e689af4b1e980
Todd Pratt <todd.pratt@cigent.com>
2021-04-24 22:51:49 -0400

    returns a token

--- server.js
+++ server.js
@@ -1,67 +1,68 @@
 const express = require("express");
 const bcrypt = require('bcrypt');
 const bodyParser = require('body-parser');
-const rxjs = require('rxjs');
-const operators = require('rxjs/operators');
+const jwt = require('express-jwt');
+const {UniqueConstraintError} = require("sequelize");
 const {db} = require('./models/db');
 const {User} = require('./models/user');
+const jsonwebtoken = require('jsonwebtoken');
 
 db.sync();
 
 const app = express();
 
 app.use(express.static("public"));
-app.use(bodyParser.json ());
+app.use(bodyParser.json());
 
 app.get("/", (request, response) => {
-  response.sendFile(__dirname + "/views/index.html");
+    response.sendFile(__dirname + "/views/index.html");
 });
 
 app.post("/api/v1/login", async (request, response) => {
-  const email = request.body.email;
-  const password = request.body.password;
-  if (email === undefined || password === undefined) {
-      response.sendStatus(400);
-      return;
-  }
-  const whereClause = { email };
-  rxjs.from(User.findAll({where: {email}})).subscribe(
-      users => {
-          if (users.length !== 1) {
-              response.sendStatus(500);
-          } else if (bcrypt.compareSync(password, users[0].get('hash'))) {
-              // send token
-
-          } else {
-              response.sendStatus(401);
-          }
-      },
-      error => {
-          response.sendStatus(500);
-      }
-  );
+    const email = request.body.email;
+    const password = request.body.password;
+    if (email === undefined || password === undefined) {
+        response.sendStatus(400);
+        return;
+    }
+    const users = await User.findAll({where: {email}});
+    if (users.length === 1) {
+        const user = users[0];
+        if (await bcrypt.compare(password, user.get('hash'))) {
+            const token = jsonwebtoken.sign({userId: user.get('id')}, process.env.SECRET, { expiresIn: '1800s' });
+            response.json({token});
+        } else {
+            response.sendStatus(401);
+        }
+    } else if (users.length === 0) {
+        response.sendStatus(401);
+    } else {
+        response.sendStatus(500);
+    }
 });
 
-app.post("/api/v1/register", (request, response) => {
-  const email = request.body.email;
-  const password = request.body.password;
-  if (email === undefined || password === undefined) {
-      response.sendStatus(400);
-      return;
-  }
-  const hash = bcrypt.hashSync(password, 10);
-  rxjs.from(User.create({email, hash}))
-      .subscribe(
-          result => {
-            response.json({success: true});
-          },
-          error => {
+app.post("/api/v1/register", async (request, response) => {
+    const email = request.body.email;
+    const password = request.body.password;
+    if (email === undefined || password === undefined) {
+        response.sendStatus(400);
+        return;
+    }
+    const hash = await bcrypt.hash(password, 10);
+    try {
+        const newUser = await User.create({email, hash});
+        response.json({success: true, newId: newUser.get('id')});
+    } catch (e) {
+        if (e instanceof UniqueConstraintError) {
+            response.sendStatus(409);
+        } else {
+            console.error(e);
             response.sendStatus(500);
-          }
-      );
+        }
+    }
 });
 
 // listen for requests :)
 const listener = app.listen(process.env.PORT, () => {
-  console.log("Your app is listening on port " + listener.address().port);
+    console.log("Your app is listening on port " + listener.address().port);
 });
