e73f652855f31a602878ca80385d645e6d59e87f
Todd Pratt <todd.pratt@cigent.com>
2021-04-26 06:59:43 -0400

    remove form labels; add some error handling.

--- public/login-form.js
+++ public/login-form.js
@@ -1,27 +1,33 @@
 Vue.component('login-form', {
     template: `
         <form>
-            <label for="email">email</label>
-            <input id="email" type="text" v-model="email" placeholder="email">
-            <label for="password">Password</label>
-            <input id="password" type="password" v-model="password" placeholder="password">
+            <input type="text" v-model="email" placeholder="email">
+            <input type="password" v-model="password" placeholder="password">
             <button type="button" v-on:click="submit">Login</button>
         </form>
+        <div class="error" v-if="error != null">
+            {{error}}
+        </div>
     `,
     data: () => ({
         email: '',
-        password: ''
+        password: '',
+        error: null
     }),
     methods: {
         submit: async event => {
-            const response = await fetch('/api/v1/login', {
-                headers: {'Content-Type': 'application/json'},
-                method: 'POST',
-                body: JSON.stringify({
-                    email: this.email.value,
-                    password: this.password.value
-                })
-            });
+            try {
+                const response = await fetch('/api/v1/login', {
+                    headers: {'Content-Type': 'application/json'},
+                    method: 'POST',
+                    body: JSON.stringify({
+                        email: this.email.value,
+                        password: this.password.value
+                    })
+                });
+            } catch (e) {
+                this.error = e;
+            }
             const data = await response.json();
             app.user_id = data.user_id;
             app.token = data.token;
--- public/register-form.js
+++ public/register-form.js
@@ -1,37 +1,38 @@
 Vue.component('register-form', {
     template: `
         <form>
-            <label for="email">email</label>
-            <input id="email" type="text" v-model="email" placeholder="E-Mail">
-            <label for="password1">Password</label>
-            <input id="password1" type="password" v-model="password1" placeholder="Password">
-            <label for="password">Password</label>
-            <input id="password2" type="password" v-model="password2" placeholder="Confirm Password">
+            <input type="text" v-model="email" placeholder="E-Mail">
+            <input type="password" v-model="password1" placeholder="Password">
+            <input type="password" v-model="password2" placeholder="Confirm Password">
             <div v-if="password1 !== password2" class="error">Passwords Must Match!</div>
             <button type="button" v-on:click="submit">Register</button>
         </form>
-        <div>
-            <div>Registration Succeeded!</div>
-            <login-form></login-form>
+        <div class="error" v-if="error != null">
+            {{error}}
         </div>
     `,
     data: () => ({
         email: '',
         password1: '',
         password2: '',
-        success: false
+        success: false,
+        error: null
     }),
     methods: {
         submit: async event => {
             if (this.password1.value === this.password2.value) {
-                const response = await fetch('/api/v1/register', {
-                    headers: {'Content-Type': 'application/json'},
-                    method: 'POST',
-                    body: JSON.stringify({
-                        email: this.email.value,
-                        password: this.password1.value
-                    })
-                });
+                try {
+                    const response = await fetch('/api/v1/register', {
+                        headers: {'Content-Type': 'application/json'},
+                        method: 'POST',
+                        body: JSON.stringify({
+                            email: this.email.value,
+                            password: this.password1.value
+                        })
+                    });
+                } catch (e) {
+                    this.error = e;
+                }
                 const data = await response.json();
                 app.page = 'login';
             }
